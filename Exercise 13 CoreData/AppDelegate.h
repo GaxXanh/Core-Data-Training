35d86a87d8c5ec3314bc931db9914b4e017d76cc
Pham Anh <anhapo11@gmail.com>
2016-03-26 13:07:05 +0700

    Initial Commit

---
+++ Exercise 13 CoreData/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Exercise 13 CoreData
+//
+//  Created by Pham Anh on 3/26/16.
+//  Copyright © 2016 com.gaxxanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Exercise 13 CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exercise 13 CoreData
+//
+//  Created by Pham Anh on 3/26/16.
+//  Copyright © 2016 com.gaxxanh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
